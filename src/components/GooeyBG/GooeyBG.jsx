dd888838dbeab279886a09880b8efb3a458f1301
Emily Bennett <emily.bennett@northcoders.com>
2022-06-23 13:44:13 +0100

    randomising animation

--- src/components/GooeyBG/GooeyBG.jsx
+++ src/components/GooeyBG/GooeyBG.jsx
@@ -3,14 +3,43 @@ import Goo from "gooey-react";
 
 function GooeyBG() {
   function generateRandomCY() {
+    const rand = Math.random() * 50;
+
+    return `${rand + 130}vh`;
+  }
+  function generateRandomSize() {
     const rand = Math.random() * 15;
 
-    return `${rand + 180}vh`;
+    return `${rand + 5}vw`;
   }
 
-  const color1 = "#ffbe0b";
-  const color2 = "#ff006e";
+  function generateRandomAnimation() {
+    const possibles = [
+      "vert8",
+      "vert10",
+      "vert12",
+      "diagR8",
+      "diagR10",
+      "diagR12",
+      "diagL8",
+      "diagL10",
+      "diagL12",
+    ];
+    const rand = Math.floor(Math.random() * possibles.length);
 
+    return possibles[rand];
+  }
+  // function generateColor() {
+  //   const possibles = [];
+  //   const rand = Math.floor(Math.random() * possibles.length);
+
+  //   return possibles[rand];
+  // }
+
+  // const color1 = "#ffbe0b";
+  // const color2 = "#ff006e";
+  const color1 = "#F4D06F";
+  const color2 = "#FF9933";
   return (
     <Goo width="100vw" height="100vh" className="background-goo">
       <svg
@@ -20,259 +49,252 @@ function GooeyBG() {
       >
         {/* verticals */}
         <circle
-          cx="12%"
+          cx="29%"
           cy={generateRandomCY()}
           fill={color1}
-          r="11vw"
-          className="vert8"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="85%"
           cy={generateRandomCY()}
           fill={color1}
-          r="13vw"
-          className="vert8"
-        />
-        <circle
-          cx="29%"
-          cy={generateRandomCY()}
-          fill={color1}
-          r="12vw"
-          className="vert8"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="87%"
           cy={generateRandomCY()}
           fill={color1}
-          r="27vw"
-          className="vert12"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="4%"
           cy={generateRandomCY()}
           fill={color1}
-          r="14vw"
-          className="vert8"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="15%"
           cy={generateRandomCY()}
           fill={color1}
-          r="25vw"
-          className="vert12"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="60%"
           cy={generateRandomCY()}
           fill={color2}
-          r="15vw"
-          className="vert10"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
-        <circle
+        {/* <circle
           cx="20%"
           cy={generateRandomCY()}
           fill={color2}
-          r="15vw"
+          r={generateRandomSize()}
           className="vert10"
-        />
+        /> */}
         <circle
           cx="66%"
           cy={generateRandomCY()}
           fill={color2}
-          r="23vw"
-          className="vert12"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         {/* <circle
           cx="49%"
           cy={generateRandomCY()}
           fill={color2}
-          r="15vw"
-          className="vert8"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="1%"
           cy={generateRandomCY()}
           fill={color2}
-          r="15vw"
+          r={generateRandomSize()}
           className="vert10"
         />
         <circle
           cx="43%"
           cy={generateRandomCY()}
           fill={color2}
-          r="25vw"
-          className="vert12"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="78%"
           cy={generateRandomCY()}
           fill={color2}
-          r="25vw"
-          className="vert12"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         /> */}
         {/* diagonals right*/}
         <circle
           cx="60%"
           cy={generateRandomCY()}
           fill={color2}
-          r="15vw"
-          className="diagR8"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="66%"
           cy={generateRandomCY()}
           fill={color2}
-          r="13vw"
-          className="diagR10"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="49%"
           cy={generateRandomCY()}
           fill={color2}
-          r="11vw"
-          className="diagR12"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />{" "}
         /
         <circle
           cx="1%"
           cy={generateRandomCY()}
           fill={color2}
-          r="11vw"
-          className="diagR8"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="43%"
           cy={generateRandomCY()}
           fill={color2}
-          r="11vw"
-          className="diagR12"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="78%"
           cy={generateRandomCY()}
           fill={color2}
-          r="11vw"
-          className="diagR10"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="85%"
           cy={generateRandomCY()}
           fill={color1}
-          r="19vw"
-          className="diagR10"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         {/* <circle
           cx="29%"
           cy={generateRandomCY()}
           fill={color1}
-          r="11vw"
-          className="diagR8"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="87%"
           cy={generateRandomCY()}
           fill={color1}
-          r="17vw"
-          className="diagR12"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="12%"
           cy={generateRandomCY()}
           fill={color1}
-          r="11vw"
-          className="diagR10"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="4%"
           cy={generateRandomCY()}
           fill={color1}
-          r="14vw"
-          className="diagR10"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="15%"
           cy={generateRandomCY()}
           fill={color1}
-          r="15vw"
-          className="diagR12"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="20%"
           cy={generateRandomCY()}
           fill={color2}
-          r="15vw"
-          className="diagR8"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         /> */}
         {/* diagonal left */}
         <circle
           cx="1%"
           cy={generateRandomCY()}
           fill={color2}
-          r="11vw"
-          className="diagL8"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="43%"
           cy={generateRandomCY()}
           fill={color2}
-          r="11vw"
-          className="diagL10"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="78%"
           cy={generateRandomCY()}
           fill={color2}
-          r="11vw"
-          className="diagL8"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="85%"
           cy={generateRandomCY()}
           fill={color1}
-          r="19vw"
-          className="diagL10"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="29%"
           cy={generateRandomCY()}
           fill={color1}
-          r="11vw"
-          className="diagL8"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="87%"
           cy={generateRandomCY()}
           fill={color1}
-          r="17vw"
-          className="diagL8"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         {/* <circle
           cx="12%"
           cy={generateRandomCY()}
           fill={color1}
-          r="11vw"
-          className="diagL8"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="4%"
           cy={generateRandomCY()}
           fill={color1}
-          r="14vw"
-          className="diagL8"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="15%"
           cy={generateRandomCY()}
           fill={color1}
-          r="15vw"
-          className="diagL8"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
         <circle
           cx="20%"
           cy={generateRandomCY()}
           fill={color2}
-          r="15vw"
-          className="diagL8"
+          r={generateRandomSize()}
+          className={generateRandomAnimation()}
         />
            */}
       </svg>
